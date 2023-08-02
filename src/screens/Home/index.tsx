2fed777a6b7f1450722755a39a6a44441610e370
lpsgabriel <gabriel.lpst7@gmail.com>
2023-08-02 00:18:56 -0300

    First Commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,121 @@
+import {
+  Alert,
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { styles } from "./styles";
+import { Participant } from "../../components/Participant";
+import { useState } from "react";
+
+const actualDate = new Date();
+const weekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
+const months = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
+const actualWeekDay = weekDays[actualDate.getDay()];
+const actualMonth = months[actualDate.getMonth()];
+
+export function Home() {
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState<string>("");
+
+  function handleParticipantAdd() {
+    if (participants.includes(participantName)) {
+      return Alert.alert(
+        "Participante existe",
+        "Este participante já foi adicionado à lista!",
+        [
+          {
+            text: "OK",
+            onPress: () => {
+              setParticipantName("");
+            },
+          },
+        ]
+      );
+    }
+    setParticipants([...participants, participantName]);
+    setParticipantName("");
+  }
+
+  function handleParticipantRemove(name: string) {
+    Alert.alert("Remover participante", `Remover o participante ${name}?`, [
+      {
+        text: "Sim",
+        onPress: () => {
+          setParticipants(
+            participants.filter((participant) => participant !== name)
+          );
+        },
+      },
+      {
+        text: "Não",
+        style: "cancel",
+      },
+    ]);
+    console.log(`removed ${name}`);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text
+        style={styles.eventDate}
+      >{`${actualWeekDay}, ${actualDate.getDate()} de ${actualMonth} de ${actualDate.getFullYear()} `}</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor={"#6b6b6b"}
+          onChangeText={setParticipantName}
+          value={participantName}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleParticipantAdd()}
+        >
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={participants}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove(item)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou ao evento ainda?{"\n"} Adicione Participantes
+          </Text>
+        )}
+      />
+    </View>
+  );
+}
